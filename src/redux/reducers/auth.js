617e1f7ab48842abf62b8a90ba680ba292f143ec
Shahbaz <94215091+shahbaz-alam7@users.noreply.github.com>
2023-01-26 23:32:19 +0530

    Update auth.js

--- src/redux/reducers/auth.js
+++ src/redux/reducers/auth.js
@@ -3,9 +3,9 @@ import { LOGOUT, AUTH } from "../../constants/actionTypes";
 const reducers = (state = { authData: null }, action) => {
   switch (action.type) {
     case AUTH:
-      localStorage.setItem("profile", JSON.stringify({ ...action?.data }));
+      localStorage.setItem("profile", JSON.stringify({ ...action.payload }));
 
-      return { ...state, authData: action.payload };
+      return { ...state, authData: action.data, loading: false, errors: null };
     case LOGOUT:
       localStorage.removeItem("profile");
       localStorage.clear();
@@ -15,20 +15,5 @@ const reducers = (state = { authData: null }, action) => {
       return state;
   }
 };
-// const authReducer = (state = { authData: null }, action) => {
-//   switch (action.type) {
-//     case AUTH:
-//       console.log(`Auth ${action.paylaod}`);
-// localStorage.setItem("profile", JSON.stringify({ ...action?.data }));
-
-//       return { ...state, authData: action.data, loading: false, errors: null };
-//     case LOGOUT:
-//       localStorage.clear();
-
-//       return { ...state, authData: null, loading: false, errors: null };
-//     default:
-//       return state;
-//   }
-// };
 
 export default reducers;
