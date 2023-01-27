c7491ee28c60901f69b3d58719a0561e83d98773
Shahbaz <94215091+shahbaz-alam7@users.noreply.github.com>
2023-01-27 16:29:43 +0530

    Update index.js

--- src/api/index.js
+++ src/api/index.js
@@ -1,7 +1,8 @@
 import axios from "axios";
 // const API = axios.create({ baseURL: "http://localhost:5000" });
 const API = axios.create({
-  baseURL: "https://memories-backend-production-e1c0.up.railway.app",
+//   baseURL: "https://memories-backend-production-e1c0.up.railway.app",
+  baseURL:"https://memories-backend-knxx.onrender.com"
 });
 
 API.interceptors.request.use((req) => {
